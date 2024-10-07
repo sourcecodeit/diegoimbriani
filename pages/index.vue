af20bcd686977a5ec82108b3a312c25a122e5234
Diego Imbriani <diego@sourcecode.team>
2024-10-07 08:45:52 +0200

    some nerdy onscroll and hover effects

--- pages/index.vue
+++ pages/index.vue
@@ -18,4 +18,33 @@ useHead({
 		{ name: 'description', content: 'Website of Diego Imbriani, with my work experience, skills and contact details.' },
 	],
 })
+
+if (import.meta.client) {
+	const sections = document.querySelectorAll('section');
+	let latestSectionIndex = -1;
+
+	window.addEventListener('scroll', function () {
+
+		// detect the "section" element that is currently in view		
+		let currentSection = sections[0];
+		let currentSectionIndex = -1;
+		for (let i = 0; i < sections.length; i++) {
+			if (window.scrollY >= (sections[i].offsetTop - 400)) {
+				currentSection = sections[i];
+				currentSectionIndex = i;
+			}
+		}
+
+		if (currentSectionIndex === latestSectionIndex) return;
+
+		console.log(currentSectionIndex, latestSectionIndex);
+
+		latestSectionIndex = currentSectionIndex;
+		// remove thee "section--active" class from all sections
+		sections.forEach(section => section.classList.remove('section--active'));
+
+		// add the "active" class to the current section
+		currentSection.classList.add('section--active');
+	});
+}
 </script>
