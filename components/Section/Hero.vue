6d6fa3c5b5749a8226c5bcd1a3f949f73d552153
Diego Imbriani <diego@sourcecode.team>
2025-10-11 13:31:25 +0200

    chore: fix style

--- components/Section/Hero.vue
+++ components/Section/Hero.vue
@@ -4,7 +4,8 @@
 			class="w-[120px] h-[120px] xl:w-[300px] xl:h-[300px] rounded-full" />
 		<div>
 			<p class="xl:text-3xl text-xl font-bold uppercase xl:mb-10">Diego Imbriani</p>
-			<h2 class="xl:text-[60px] text-2xl text-zinc-400 uppercase font-bold xl:mb-10">Senior Full Stack Developer | Cloud & System Design</h2>
+			<h2 class="xl:text-[40px] text-2xl text-zinc-400 uppercase font-bold xl:mb-4">Senior Full Stack Developer</h2>
+			<h2 class="xl:text-[40px] text-2xl text-zinc-400 uppercase font-bold xl:mb-10">Cloud & System Design</h2>
 			<div class="flex gap-4 mt-4 flex-wrap">
 				<UiButton to="#contacts" highlight>{{ $t('hero.contact') }}
 					<Icon name="mdi:arrow-top-right" />
